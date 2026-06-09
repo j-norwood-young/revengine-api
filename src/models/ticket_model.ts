7a1b351e42c2e1470613ea94660bedded085e170
Jason Norwood-Young <jason@10layer.com>
2026-06-09 09:34:31 +0200

    New "ticket" model

---
+++ src/models/ticket_model.ts
@@ -0,0 +1,25 @@
+import "jxp/globals";
+/* global JXPSchema ObjectId Mixed */
+
+const TicketSchema = new JXPSchema({
+    reader_id: { type: ObjectId, link: "reader", index: true },
+    event: { type: String, index: true },
+    event_date: { type: Date, index: true },
+    booking_date: { type: Date, index: true },
+    seats: [String],
+    value: { type: Number, index: true },
+    vendor: { type: String, index: true },
+    payload: Mixed,
+},
+{
+    perms: {
+        admin: "crud",
+        owner: "crud",
+        user: "cr"
+    }
+});
+
+TicketSchema.index({ reader_id: 1, event_date: -1 });
+
+const Ticket = JXPSchema.model('ticket', TicketSchema);
+export = Ticket;
